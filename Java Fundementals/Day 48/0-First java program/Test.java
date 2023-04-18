4d84c99e73560b52ad76e5a0909a55fbb7948433
Sara <saranatourcontact@gmail.com>
2023-04-18 14:48:09 +0300

    first java program

---
+++ Java Fundementals/Day 48/0-First java program/Test.java
@@ -0,0 +1,12 @@
+// My name is Coding Dojo
+// I am 100 years old
+// My hometown is Burbank, CA
+
+public class Test {
+	public static void main(String[] args) {
+    String name ="Sara";
+    int age =23;
+    String homeTown="Tulkarem";
+    System.out.println("My name is: " + name +'\n' + "I am " + age +'\n'+ "My hommetown is " +homeTown);
+	}
+}
