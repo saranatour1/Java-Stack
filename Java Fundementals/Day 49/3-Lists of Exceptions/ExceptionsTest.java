6dfa650c0de38f5f8024e6f498338810b5604dc8
Sara <saranatourcontact@gmail.com>
2023-04-19 16:49:11 +0300

    Lists of Exceptions assignmeent

---
+++ Java Fundementals/Day 49/3-Lists of Exceptions/ExceptionsTest.java
@@ -0,0 +1,23 @@
+
+public class ExceptionsTest {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    Exceptions appTest = new Exceptions();
+    // HashMap<String, String> trackList = appTest.getTrackList();
+
+    Exceptions.tryingExceptions();
+
+
+  }
+}
+
+/*
+ * 
+ *Exception in thread "main" java.lang.ClassCastException: class java.lang.
+ String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are
+ * 
+ * 
+ */
