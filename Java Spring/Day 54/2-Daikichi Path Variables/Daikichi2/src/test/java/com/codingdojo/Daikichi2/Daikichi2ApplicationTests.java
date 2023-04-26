156c87c74a3955fbea67950f2d1dd431a7f51204
Sara <saranatourcontact@gmail.com>
2023-04-26 14:17:47 +0300

    Path variables assigment

---
+++ Java Spring/Day 54/2-Daikichi Path Variables/Daikichi2/src/test/java/com/codingdojo/Daikichi2/Daikichi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Daikichi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daikichi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
