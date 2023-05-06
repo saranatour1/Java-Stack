1c7d82eb4b5355588ea1c0fe59a6f94c8a8b9e76
Sara <saranatourcontact@gmail.com>
2023-05-06 16:12:54 +0300

    missing import

--- spring adding one to many/Day 60/0-Dojos and ninjas/DojosandNinjas/src/main/java/com/codingdojo/mvc/repositories/NinjaRepository.java
+++ spring adding one to many/Day 60/0-Dojos and ninjas/DojosandNinjas/src/main/java/com/codingdojo/mvc/repositories/NinjaRepository.java
@@ -3,6 +3,7 @@ package com.codingdojo.mvc.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.codingdojo.mvc.models.Ninja;
 @Repository
