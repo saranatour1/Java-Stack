2f1fd8b2d2f644e67491439549b0de09342b961b
Sara <saranatourcontact@gmail.com>
2023-05-30 21:18:09 +0300

    Ai

--- Artificial intellegance session/mysite/polls/views.py
+++ Artificial intellegance session/mysite/polls/views.py
@@ -7,36 +7,21 @@ from textblob import TextBlob
 
 def index(request):
     num_visits = request.session.get('blob')
-    
     context = {'h':num_visits}
     return render(request, "chat.html" , context=context)
 
 def formInput(request):
     kIn = request.POST['thisInput']
-    print(request.POST['thisInput'])
+    # print(request.POST['thisInput'])
     blob = TextBlob(kIn)
     sentiment = blob.sentiment.polarity
+    # sentiment = str(blob)
     print(sentiment)
     request.session["blob"] = sentiment
     return redirect("/")
 
 
 
-# # Define a sample text
-# text = """I love this product.
-# It is the best purchase 
-# I have ever made."""
-# text1='I will burn the world'
-# text2='I hate the world'
-# #text="hate the world"
-# # Create a TextBlob object for the text
-# blob = TextBlob(text2)
-
-# # Perform sentiment analysis on the text
-# sentiment = blob.sentiment.polarity
-
-# # Print the sentiment score
-# print(sentiment)
 
 
 
